b8cac04b9ffcd86b38ba22584d8b3d13aac894b0
finn <nvtuongcs@gmail.com>
2020-05-24 10:17:01 +0700

    Refactor session

--- src/session/index.js
+++ src/session/index.js
@@ -1,32 +1,27 @@
-'use strict';
+import session from "express-session";
+import connectMongo from "connect-mongo";
+import db from "database";
+import config from "config";
 
-var session 	= require('express-session');
-var MongoStore	= require('connect-mongo')(session);
-var db 		    = require('../database');
-var config 		= require('../config');
+const MongoStore = connectMongo(session);
 
-/**
- * Initialize Session
- * Uses MongoDB-based session store
- *
- */
-var init = function () {
-	if(process.env.NODE_ENV === 'production') {
-		return session({
-			secret: config.sessionSecret,
-			resave: false,
-			saveUninitialized: false,
-			unset: 'destroy',
-			store: new MongoStore({ mongooseConnection: db.Mongoose.connection })
-		});
-	} else {
-		return session({
-			secret: config.sessionSecret,
-			resave: false,
-			unset: 'destroy',
-			saveUninitialized: true
-		});
-	}
-}
+const init = function () {
+  if (process.env.NODE_ENV === "production") {
+    return session({
+      secret: config.sessionSecret,
+      resave: false,
+      saveUninitialized: false,
+      unset: "destroy",
+      store: new MongoStore({ mongooseConnection: db.Mongoose.connection }),
+    });
+  } else {
+    return session({
+      secret: config.sessionSecret,
+      resave: false,
+      unset: "destroy",
+      saveUninitialized: true,
+    });
+  }
+};
 
-module.exports = init();
+export default init();
