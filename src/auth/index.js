4d7baeb9c0a4401629ff499ae75174a939461d06
finn <nvtuongcs@gmail.com>
2020-05-24 09:54:45 +0700

    Refactor auth

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,69 +1,76 @@
-'use strict';
+import passport from "passport";
+import PassportLocal from "passport-local";
+import PassportFacebook from "passport-facebook";
+import PassportTwitter from "passport-twitter";
+import User from "models/user";
+import config from "config";
+import logger from "logger";
 
-var config 		= require('../config');
-var passport 	= require('passport');
-var logger 		= require('../logger');
+const LocalStrategy = PassportLocal.Strategy;
+const FacebookStrategy = PassportFacebook.Strategy;
+const TwitterStrategy = PassportTwitter.Strategy;
 
-var LocalStrategy 		= require('passport-local').Strategy;
-var FacebookStrategy  	= require('passport-facebook').Strategy;
-var TwitterStrategy  	= require('passport-twitter').Strategy;
+const init = function () {
+  logger.info("Oauth init");
+  // Serialize and Deserialize user instances to and from the session.
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
 
-var User = require('../models/user');
+  passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+      done(err, user);
+    });
+  });
 
-/**
- * Encapsulates all code for authentication 
- * Either by using username and password, or by using social accounts
- *
- */
-var init = function(){
+  // Plug-in Local Strategy
+  passport.use(
+    new LocalStrategy(function (username, password, done) {
+      User.findOne(
+        { username: new RegExp(username, "i"), socialId: null },
+        function (err, user) {
+          if (err) {
+            return done(err);
+          }
 
-	// Serialize and Deserialize user instances to and from the session.
-	passport.serializeUser(function(user, done) {
-		done(null, user.id);
-	});
+          if (!user) {
+            return done(null, false, {
+              message: "Incorrect username or password.",
+            });
+          }
 
-	passport.deserializeUser(function(id, done) {
-		User.findById(id, function (err, user) {
-			done(err, user);
-		});
-	});
+          user.validatePassword(password, function (err, isMatch) {
+            if (err) {
+              return done(err);
+            }
+            if (!isMatch) {
+              return done(null, false, {
+                message: "Incorrect username or password.",
+              });
+            }
+            return done(null, user);
+          });
+        }
+      );
+    })
+  );
 
-	// Plug-in Local Strategy
-	passport.use(new LocalStrategy(
-	  function(username, password, done) {
-	    User.findOne({ username: new RegExp(username, 'i'), socialId: null }, function(err, user) {
-	      if (err) { return done(err); }
+  // In case of Facebook, tokenA is the access token, while tokenB is the refersh token.
+  // In case of Twitter, tokenA is the token, whilet tokenB is the tokenSecret.
+  const verifySocialAccount = function (tokenA, tokenB, data, done) {
+    User.findOrCreate(data, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+      return done(err, user);
+    });
+  };
 
-	      if (!user) {
-	        return done(null, false, { message: 'Incorrect username or password.' });
-	      }
+  // Plug-in Facebook & Twitter Strategies
+  passport.use(new FacebookStrategy(config.facebook, verifySocialAccount));
+  passport.use(new TwitterStrategy(config.twitter, verifySocialAccount));
 
-	      user.validatePassword(password, function(err, isMatch) {
-	        	if (err) { return done(err); }
-	        	if (!isMatch){
-	        		return done(null, false, { message: 'Incorrect username or password.' });
-	        	}
-	        	return done(null, user);
-	      });
+  return passport;
+};
 
-	    });
-	  }
-	));
-
-	// In case of Facebook, tokenA is the access token, while tokenB is the refersh token.
-	// In case of Twitter, tokenA is the token, whilet tokenB is the tokenSecret.
-	var verifySocialAccount = function(tokenA, tokenB, data, done) {
-		User.findOrCreate(data, function (err, user) {
-	      	if (err) { return done(err); }
-			return done(err, user); 
-		});
-	};
-
-	// Plug-in Facebook & Twitter Strategies
-	passport.use(new FacebookStrategy(config.facebook, verifySocialAccount));
-	passport.use(new TwitterStrategy(config.twitter, verifySocialAccount));
-
-	return passport;
-}
-	
-module.exports = init();
+export default init();
