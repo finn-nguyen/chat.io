57ee064fa746daa100c6e682216c38d81df90b3c
finn <nvtuongcs@gmail.com>
2020-05-26 20:20:27 +0700

    Init chat_io db

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import app from 'app';
+import db from 'db';
 import config from 'config';
 import logger from 'logger';
 import createHttpServer from 'httpServer';
@@ -8,10 +9,20 @@ import createSocketServer from 'socketServer';
 const httpServer = createHttpServer(app);
 const socketServer = createSocketServer(app);
 
-httpServer.listen(config.httpPort, () => {
-  logger.info(`Http server is listening at port ${config.httpPort}`);
-});
+const bootstrapAsync = async () => {
+  try {
+    await db.authenticate();
 
-socketServer.listen(config.socketPort, () => {
-  logger.info(`Socket server is listening at port ${config.socketPort}`);
-});
+    httpServer.listen(config.httpPort, () => {
+      logger.info(`Http server is listening at port ${config.httpPort}`);
+    });
+
+    socketServer.listen(config.socketPort, () => {
+      logger.info(`Socket server is listening at port ${config.socketPort}`);
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+bootstrapAsync();
